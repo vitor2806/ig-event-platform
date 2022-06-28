81f54296611f36b4f1dd4a8ced7f4dc33d2990f1
Vitor Rafael <rqfvitor@gmail.com>
2022-06-28 15:05:12 -0300

    add background to home

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
+import { GithubLogo } from 'phosphor-react';
+
 export function Footer() {
   return (
-    <div>
-      <p>Footer</p>
+    <div className="bg-gray-700 border-t-2 border-gray-600 w-full fixed bottom-0 p-5 flex items-center gap-6">
+      <a href="https://github.com/vitor2806" target={'_blank'} className="flex items-center gap-2">
+        <GithubLogo size={26} />
+        <span className="text-lg font-medium">Vitor Rafael</span>
+      </a>
+      <div className="flex justify-between flex-1 text-gray-300 text-xs">
+        <span>All rights reserved</span>
+        <span>Privacy Policy</span>
+      </div>
     </div>
   );
 }
