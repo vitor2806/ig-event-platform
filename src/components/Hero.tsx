21c75fcdb84a3889dd265072fda4af3fce6c3fee
Vitor Rafael <rqfvitor@gmail.com>
2022-06-30 15:12:58 -0300

    add home page responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,13 +3,13 @@ import { Logo } from './Logo';
 
 export function Hero() {
   return (
-    <div className="flex items-center justify-center w-full gap-52">
+    <div className="flex px-10 items-center justify-center w-full gap-20 flex-col lg:flex-row">
       <div className="flex flex-col max-w-[624px]">
         <Logo />
-        <h1 className="text-5xl mt-10">
+        <h1 className="text-4xl xl:text-5xl mt-10 leading-tight">
           Build a <span className="text-blue-500 font-medium">real application</span> from scratch using <span className="text-blue-500 font-medium">React JS</span>
         </h1>
-        <span className="text-md text-gray-200 mt-6">In only a week you will domain one of the most used technologies in the web world. You will learn great concepts of react coding, as well good view of GraphQL.</span>
+        <span className="text-sm lg:text-md text-gray-200 mt-4 leading-relaxed">In only a week you will domain one of the most used technologies in the web world. You will learn great concepts of react coding, as well good view of GraphQL.</span>
       </div>
       <Form />
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
 import { Footer } from '../components/Footer';
 import { Hero } from '../components/Hero';
-import HeroImg from '../components/HeroImg';
 
 export function Home() {
   return (
-    <div className="flex flex-col items-center py-[120px] bg-[url('../assets/blur.svg')] bg-cover bg-blur bg-no-repeat">
+    <div className="min-h-screen flex flex-col items-center py-[120px] bg-[url('../assets/blur.svg')] bg-cover bg-blur bg-no-repeat">
       <Hero />
-      <HeroImg />
+      <img src="src/assets/background-signup.png" alt="Technologies floating in vscode" className="mt-10" />
       <Footer />
     </div>
   );
